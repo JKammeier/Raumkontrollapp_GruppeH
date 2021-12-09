de8f896f988191a73bfac3e754a8f70dcd1dd9b8
Jannis Kammeier <jannis.kammeier@fh-bielefeld.de>
2021-12-09 11:26:42 +0100

    MainActivity ursprüngliches Setup + Raum Parcelable gemacht

--- app/src/main/java/de/fhbielefeld/swe/raumkontrollapp_h/MainActivity.java
+++ app/src/main/java/de/fhbielefeld/swe/raumkontrollapp_h/MainActivity.java
@@ -2,13 +2,52 @@ package de.fhbielefeld.swe.raumkontrollapp_h;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Raum> raumListe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonOpen = findViewById(R.id.ButtonOpen);
+        EditText eingabeRaumNr = findViewById(R.id.EingabeRaumNr);
+
+        buttonOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                eingabeRaumNr.setText("");
+                Raum aktuellerRaum = getRaum(eingabeRaumNr.getText().toString());
+
+                Intent raumAkt = new Intent(MainActivity.this, RaumActivity.class);
+                raumAkt.putExtra("Raum", aktuellerRaum);
+                startActivity(raumAkt);
+                // in RaumActivity: aktuellerRaum = getIntent().getExtras().getParcelable("Raum");
+            }
+        });
+    }
+
+    public Raum getRaum (String raumNr) {
+        for (Raum aktuellerRaum : raumListe) {
+            if (aktuellerRaum.nr == raumNr) {
+                return aktuellerRaum;
+            }
+        }
+
+        Raum neuerRaum = new Raum();
+        neuerRaum.nr = raumNr;
+        raumListe.add(neuerRaum);
+        return neuerRaum;
     }
 }
--- app/src/main/java/de/fhbielefeld/swe/raumkontrollapp_h/Raum.java
+++ app/src/main/java/de/fhbielefeld/swe/raumkontrollapp_h/Raum.java
@@ -1,4 +1,18 @@
 package de.fhbielefeld.swe.raumkontrollapp_h;
 
-public class Raum {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Raum implements Parcelable {
+    String nr;
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(nr);
+    }
 }
