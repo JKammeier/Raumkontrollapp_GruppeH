6de68813a1c344f22b6eece38da6d897c934adf4
tresorlath <marie_tresor_oris.lath@fh-bielefeld.de>
2022-01-01 00:00:43 +0100

    Neues Jahr, neuer Start dann neues Design

--- app/src/main/java/de/fhbielefeld/swe/raumkontrollapp_h/AusstattungHinzuFragment.java
+++ app/src/main/java/de/fhbielefeld/swe/raumkontrollapp_h/AusstattungHinzuFragment.java
@@ -29,67 +29,8 @@ public class AusstattungHinzuFragment<raumnr> extends Fragment {
         super.onCreate(savedInstanceState);
         binding = FragmentAusstattungHinzuBinding.inflate(inflater, container, false);
         return binding.getRoot();
-
-
     }
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        //created method
-        setContentView(R.layout.fragment_ausstattung_hinzu);
-
-        TextView rn = findViewById(R.id.RaumnummerHinzu);
-
-        TextView fg = findViewById(R.id.FragetextWasHinzu);
-        TextView an = findViewById(R.id.EingabeAnzahlHinzu);
-        //casting vorgeschlagen
-        EditText so = (EditText) findViewById(R.id.sonstiges);
-        //cast
-        Button fu = (Button) findViewById(R.id.buttonfürsSonstiges);
-
-
-// von OK Button zurück zu raumactivity
-        fu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                so.setText(so.getText().toString());
-                setContentView(R.layout.activity_raum);
-
-            }
-
-
-        });
-
-
-
-
-
-    }
-
-
-//methode
-    private TextView findViewById(int raumnummerHinzu) {
-        return null;
-    }
-
-
-
-    private void setContentView(int fragment_ausstattung_hinzu) {
-    }
-
-    /*
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(AusstattungHinzuFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
-            }
-        });
-*/
 
     @Override
     public void onDestroyView() {
