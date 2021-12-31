179a63fb5e2b307792594a2670e104826a3c2dcb
tresorlath <marie_tresor_oris.lath@fh-bielefeld.de>
2021-12-31 04:01:42 +0100

    klasse AusstattungHinzuFragment

--- app/src/main/java/de/fhbielefeld/swe/raumkontrollapp_h/AusstattungHinzuFragment.java
+++ app/src/main/java/de/fhbielefeld/swe/raumkontrollapp_h/AusstattungHinzuFragment.java
@@ -4,8 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
@@ -27,7 +32,46 @@ public class AusstattungHinzuFragment<raumnr> extends Fragment {
         return binding.getRoot();
 
     }
-/*
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //created method
+        setContentView(R.layout.fragment_ausstattung_hinzu);
+
+        TextView rn = findViewById(R.id.RaumnummerHinzu);
+        TextView na = findViewById(R.id.NeueAusstattung);
+        TextView fg = findViewById(R.id.FragetextWasHinzu);
+        TextView an = findViewById(R.id.EingabeAnzahlHinzu);
+        //casting vorgeschlagt
+        EditText so = (EditText) findViewById(R.id.sonstiges);
+        //cast
+        Button fu = (Button) findViewById(R.id.buttonfürsSonstiges);
+
+        fu.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                so.setText(so.getText().toString());
+
+            }
+
+
+        });
+    }
+
+
+//methode
+    private TextView findViewById(int raumnummerHinzu) {
+        return null;
+    }
+
+
+    private void setContentView(int fragment_ausstattung_hinzu) {
+    }
+
+    /*
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
