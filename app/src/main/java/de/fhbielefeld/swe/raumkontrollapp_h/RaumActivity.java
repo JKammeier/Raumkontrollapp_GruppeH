b38369270cceed4551c9fce5ef8c237c941ef0fa
Jannis Kammeier <jannis.kammeier@fh-bielefeld.de>
2022-01-02 16:28:00 +0100

    MainActivity und RaumActivity aufgeräumt

--- app/src/main/java/de/fhbielefeld/swe/raumkontrollapp_h/RaumActivity.java
+++ app/src/main/java/de/fhbielefeld/swe/raumkontrollapp_h/RaumActivity.java
@@ -1,5 +1,6 @@
 package de.fhbielefeld.swe.raumkontrollapp_h;
 
+import android.app.Fragment;
 import android.content.SharedPreferences;
 import android.graphics.Paint;
 import android.os.Bundle;
@@ -26,23 +27,16 @@ import java.util.List;
 
 import de.fhbielefeld.swe.raumkontrollapp_h.databinding.ActivityRaumBinding;
 
-public class RaumActivity extends AppCompatActivity implements View.OnClickListener
+public class RaumActivity extends AppCompatActivity
 {
-    //private CollectionReference raumListeFirebase = FirebaseFirestore.getInstance().collection("raeume");
     private DocumentReference raum;
     private ArrayList<String> ausstattungsListe;
     private ArrayAdapter arrayAdapter;
 
-    private Raum aktuellerRaum;
-    String prefNeueAusstattung = "NeueAussattung";
-
-    TextView textView_RaumnummerZahl, textView_ZimmergroesseZahl, textView_AnzahlStuehle,
-            textView_AnzahlTische, textView_Stuehle, textView_Tische,
-            textView_Raumnummer, textView_Eigenschaften, textView_Ausstattung;
+    TextView textView_RaumnummerZahl, textView_ZimmergroesseZahl, textView_Raumnummer, textView_Eigenschaften, textView_Ausstattung;
     FloatingActionButton fab_AusstattungHinzufuegen;
     ListView listView;
 
-    private AppBarConfiguration appBarConfiguration;
     private ActivityRaumBinding binding;
 
     private String raumNr;
@@ -55,15 +49,11 @@ public class RaumActivity extends AppCompatActivity implements View.OnClickListe
 
         textView_RaumnummerZahl = findViewById(R.id.textView_RaumnummerZahl);
         textView_ZimmergroesseZahl = findViewById(R.id.textView_ZimmergroesseZahl);
-        textView_AnzahlStuehle = findViewById(R.id.textView_AnzahlStuehle);
-        textView_AnzahlTische = findViewById(R.id.textView_AnzahlTische);
-        textView_Stuehle = findViewById(R.id.textView_Stuehle);
-        textView_Tische = findViewById(R.id.textView_Tische);
         textView_Raumnummer = findViewById(R.id.textView_Raumnummer);
         textView_Eigenschaften = findViewById(R.id.textView_Eigenschaften);
         textView_Ausstattung = findViewById(R.id.textView_Ausstattung);
         fab_AusstattungHinzufuegen = findViewById(R.id.fab_AusstattungHinzufuegen);
-        fab_AusstattungHinzufuegen.setOnClickListener(this);
+
         textView_Eigenschaften.setPaintFlags(textView_Eigenschaften.getPaintFlags()
                 |Paint.UNDERLINE_TEXT_FLAG);
         textView_Ausstattung.setPaintFlags(textView_Ausstattung.getPaintFlags()
@@ -72,16 +62,34 @@ public class RaumActivity extends AppCompatActivity implements View.OnClickListe
                 |Paint.UNDERLINE_TEXT_FLAG);
 
         listView = (ListView)findViewById(R.id.Ausstattungen_listView);
-        /*ArrayList<String> arrayList = new ArrayList<>();
-        arrayList.add("test1");
-        arrayList.add("test2");
-        arrayList.add("test2");
-        ArrayAdapter arrayAdapter = new ArrayAdapter(
-                this, android.R.layout.simple_expandable_list_item_1,arrayList);
-        listView.setAdapter(arrayAdapter);*/
+
         ausstattungsListe = new ArrayList<>();
         arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, ausstattungsListe);
         listView.setAdapter(arrayAdapter);
+
+        // Raumnummer etc. von MainActivity übergeben
+        raumNr = getIntent().getExtras().getString("RaumNr");
+        raum = FirebaseFirestore.getInstance().document("raeume/" + raumNr);
+        textView_RaumnummerZahl.setText(raum.getId());
+
+        if (getIntent().getExtras().getBoolean("raumNeu")) {
+            showPopup(raum);
+        }
+
+        raum.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if (documentSnapshot.exists()) {
+                    try {
+                        textView_ZimmergroesseZahl.setText(documentSnapshot.get("flaeche").toString());
+                    } catch (Exception e) {
+                        Log.d("EigeneException", "kein Feld flaeche vorhanden");
+                    }
+                }
+            }
+        });
+
+        getAusstattungFirebase();
 
         // Von ListView zu Ausstattung_detail
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -92,6 +100,7 @@ public class RaumActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
 
+        // Ausstattung löschen
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
@@ -110,43 +119,11 @@ public class RaumActivity extends AppCompatActivity implements View.OnClickListe
         {
             @Override
             public void onClick(View view) {
+                //AusstattungHinzuFragment ausstattungHinzuFragment = new AusstattungHinzuFragment();
+                //getSupportFragmentManager().beginTransaction().replace(R.id.linearLayout, new AusstattungHinzuFragment()).commit();
                 setContentView(R.layout.fragment_ausstattung_hinzu);
             }
         });
-
-
-        // Raumnummer etc. von MainActivity übergeben
-        //setSupportActionBar(binding.toolbar);
-        raumNr = getIntent().getExtras().getString("RaumNr");
-        raum = FirebaseFirestore.getInstance().document("raeume/" + raumNr);
-        //aktuellerRaum = getIntent().getExtras().getParcelable("Raum");
-        //textView_RaumnummerZahl.setText(aktuellerRaum.getRaumNr());
-        textView_RaumnummerZahl.setText(raum.getId());
-
-        if (getIntent().getExtras().getBoolean("raumNeu")) {
-            showPopup(raum);
-        }
-
-
-        raum.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-            @Override
-            public void onSuccess(DocumentSnapshot documentSnapshot) {
-                if (documentSnapshot.exists()) {
-                    try {
-                        textView_ZimmergroesseZahl.setText(documentSnapshot.get("flaeche").toString());
-                    } catch (Exception e) {
-                        Log.d("Exception", "Fläche holen");
-                    }
-                }
-            }
-        });
-
-        getAusstattungFirebase();
-        //textView_AnzahlStuehle.setText(aktuellerRaum.anzahl_stuehle);
-        //textView_AnzahlTische.setText(aktuellerRaum.anzahl_tische);
-        //textView_ZimmergroesseZahl.setText(aktuellerRaum.zimmergroesse_zahl);
-
-
     }
 
     private void showPopup(DocumentReference neuerRaum) {
@@ -164,71 +141,10 @@ public class RaumActivity extends AppCompatActivity implements View.OnClickListe
             public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
                 List<DocumentSnapshot> snapshotList = queryDocumentSnapshots.getDocuments();
                 for (DocumentSnapshot doc: snapshotList) {
-                    //raumListe.add(doc.toObject(Raum.class));
                     ausstattungsListe.add(doc.getId());
                 }
                 arrayAdapter.notifyDataSetChanged();
             }
         });
-
-
-    }
-
-    public void createDatabase()
-    {
-
-    }
-
-    public void loadRoom()
-    {
-
-    }
-
-    public void safeRoom()
-    {
-
-    }
-
-    // Wenn etwas in neueAusstattung hinzugefügt wurde, ist es wahr oder falsch?
-    public boolean neueAusstattung()
-    {
-        // SharedPreference: Speichern von Inhalten in prefNeueAusstattung
-        SharedPreferences preferences = getSharedPreferences(prefNeueAusstattung, MODE_PRIVATE);
-        // Wenn etwas in prefRaumListe gespeichert ist
-        if (preferences.getBoolean(prefNeueAusstattung, true))
-        {
-            // Wert (boolean) in prefNeueAussattung ändern
-            SharedPreferences.Editor editor = preferences.edit();
-            // Boolean setzen
-            editor.putBoolean(prefNeueAusstattung, false);
-            editor.commit();
-            // Neue Aussattung gefunden
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-
-    @Override
-    public void onClick(View view)
-    {
-        /*
-        switch (view.getId())
-        {
-            case R.id.fab_AusstattungHinzufuegen:
-                setContentView(R.layout.fragment_ausstattung_hinzu);
-                break;
-
-            case R.id.textView_Tische:
-                setContentView(R.layout.fragment_ausstattungs_detail);
-                break;
-
-            case R.id.textView_Stuehle:
-                setContentView(R.layout.fragment_ausstattungs_detail);
-                break;
-        }
-         */
     }
 }
