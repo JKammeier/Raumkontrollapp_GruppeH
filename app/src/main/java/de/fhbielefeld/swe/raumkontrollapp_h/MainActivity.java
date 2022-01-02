5e29e36aad708f1a7bd6e3e19c61c2c41acfd7da
tresorlath <marie_tresor_oris.lath@fh-bielefeld.de>
2022-01-02 13:30:48 +0100

    Toast nach dem Löschen einem Raum implementiert

--- app/src/main/java/de/fhbielefeld/swe/raumkontrollapp_h/MainActivity.java
+++ app/src/main/java/de/fhbielefeld/swe/raumkontrollapp_h/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCanceledListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -86,6 +87,7 @@ public class MainActivity extends AppCompatActivity {
 
                 raumNrListe.remove(position);
                 arrayAdapter.notifyDataSetChanged();
+                Toast.makeText(MainActivity.this, "Raum erfolgreich gelöscht", Toast.LENGTH_LONG).show();
 
                 return false;
             }
